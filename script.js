0b53b47380d5ff4ec11e2b1b936033c5db8032b3
merahab <ts.baran6161@gmail.com>
2024-01-31 21:56:45 +0300

    RastgeleSöz

---
+++ script.js
@@ -0,0 +1,46 @@
+const day = document.getElementById('day')
+const randomBtn = document.querySelector('.random')
+
+deneme = new Date;
+
+console.log(deneme);
+
+let sözler = [
+`Ben ağlarken yanımda yoksan, ben gülerken de gölge yapma.`,
+
+`Meğer hayat dediğin sadece gözlerinden ibaretmiş.`,
+    
+`Seni yıldızlar sönene kadar seveceğim, sevgilim.`,
+    
+`Unutulmak kadar acıdır bazen yaşamak.`,
+
+`Karakteri menfaatlerine göre şekillenen insanlar var.`,
+
+`Sen aklım ve kalbim arasında kalan en güzel çaresizliğimsin.`,
+
+`Ömrü bitene kadar sevmeli insan. Menfaatleri bitene kadar değil.`,
+
+]
+
+
+let gün = deneme.getDay()
+console.log(sözler[gün]); 
+let olay = sözler[gün]
+// console.log(gün);
+
+function günüYenile(){
+   setTimeout(() =>{
+    day.innerText = ` " ${olay} "`
+   },100)
+}
+günüYenile()
+
+randomBtn.addEventListener('click', ()=>{
+   let random = Math.floor(Math.random(sözler) * 7)
+   let rastgeleSözBtn = sözler[random];
+   let newWrite = document.querySelector('.yeni')
+
+   newWrite.innerHTML = `" ${rastgeleSözBtn} "`
+   console.log(random);
+})
+
